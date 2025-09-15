e80db43d7b6d984a7e3941be86e8a8be16b94719
arundom <arun_malapalil@hotmail.com>
2025-09-15 17:40:23 +0530

    Added tab navigation feature

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeArea } from "./src/components/utility/safe-area.component";
 
-//import { NavigationContainer } from "@react-navigation/native";
-//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-/*
 // Below code is for Navigation setup
 const Tab = createBottomTabNavigator();
 
@@ -16,28 +17,30 @@ const Settings = () => (
     <Text>Settings</Text>
   </SafeArea>
 );
+
 const Map = () => (
   <SafeArea>
     <Text>Map</Text>
   </SafeArea>
 );
-*/
+
+//<RestaurantsScreen />
 
 export default function App() {
   return (
 
     <>
      <ThemeProvider theme={theme}>
-             <RestaurantsScreen />
-        
-        {/*<NavigationContainer>
+                   
+        {<NavigationContainer>
           <Tab.Navigator>
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
           </Tab.Navigator>
         </NavigationContainer>
-      */}
+        }
+      
       </ThemeProvider>
 
       <ExpoStatusBar style="auto" />
