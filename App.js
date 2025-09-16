ae79b53ece2590d3c38b44ca041b6a4c9256e36e
arundom <arun_malapalil@hotmail.com>
2025-09-16 14:11:20 +0530

    Added icons to bottom tabs

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeArea } from "./src/components/utility/safe-area.component";
+import { Fontisto,Ionicons } from "@expo/vector-icons";
 
 
 // Below code is for Navigation setup
@@ -33,7 +34,27 @@ export default function App() {
      <ThemeProvider theme={theme}>
                    
         {<NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                if (route.name === "Restaurants") {
+                  iconName = "restaurant";
+                } else if (route.name === "Settings") {
+                  iconName = "settings";
+                } else if (route.name === "Map") {
+                  iconName = "map-marker-alt";
+                  return <Fontisto name="map-marker-alt" size={size} color={color} />;
+                }
+                //<Fontisto name="map-marker-alt" size={24} color="black" />
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "gray",
+            }}>
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
